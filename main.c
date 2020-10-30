e66b83e7cc21ca9a55693a92281d2dfed725cde9
mcb2003 <mikeybuchan@hotmail.co.uk>
2020-10-30 22:02:08 +0000

    Add licensing information.

--- main.c
+++ main.c
@@ -1,3 +1,25 @@
+/*
+    gc - Grammar Correcter
+    Copyright (C) 2020  Michael Connor Buchan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Author: Michael Connor Buchan <mikeybuchan@hotmail.co.uk>
+   */
+
+/* SPDXLicenseIdentifier: GPL-3-or-later */
+
 #include <ctype.h> // Char comparison functions
 #include <errno.h> // Error handling
 #include <locale.h> // Localisation
@@ -16,7 +38,7 @@ void help(bool usage) {
     if(usage) fprintf(stderr, "%s: No files specified\n", G_PROG_NAME);
         fprintf(stderr, "Usage: %s file1[ file2[ ...]]\n", G_PROG_NAME);
         if(!usage) {
-            fprintf(stderr, "\n%s - Grammar Correcter\n", G_PROG_NAME);
+            fprintf(stderr, "\n%s - Grammar Correcter  Copyright (C) 2020  Michael Connor Buchan\n", G_PROG_NAME);
             fprintf(stderr, "Corrects grammar and punctuation, and collapses white-space in each of the input\nfiles, outputting to STDOUT by default.\n");
             // The user just asked for help, this was a success
             exit(EXIT_SUCCESS);
